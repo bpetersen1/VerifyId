d07668693d08261d351860bd889c755362ee9a54
Bradley Petersen <petersen285@gmail.com>
2017-04-17 04:55:35 +0200

    updated packages

---
+++ VerifyId_Test/MyCreditsTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VerifyId_Test
+{
+    [TestClass]
+    public class MyCreditsTest
+    {
+        public MyCreditsTest()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+        }
+        [TestMethod]
+        public async Task TestResponseForMyCredits()
+        {
+            
+            var service = new VerifyId.VerificationService.Authenticate();
+            string apikey = await service.Excecute(GetCredentials.Username, GetCredentials.Password);
+
+            var mycredit  = new VerifyId.VerificationService.MyCredits();
+
+            var result = mycredit.GetAvailableCredits(apikey);
+
+            Assert.IsTrue(result.Result.credits > 1);
+
+        }
+    }
+}
